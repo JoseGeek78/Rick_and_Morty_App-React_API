886f33483b219e0a9d5571c758d0adb162f1b7b1
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-16 18:56:59 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
+import Navbar from './components/Navbar'
+
 function App() {
   return (
 
   <>
-  <nav className="navbar navbar-dark bg-dark">
-    <div className="container">
-      <a className="navbar-brand text-uppercase" href="http://localhost:3000/">Rick and Morty app</a>
-    </div>
-  </nav>
+  <Navbar />
     </>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+<div>
+    <nav className="navbar navbar-dark bg-dark">
+    <div className="container">
+      <a className="navbar-brand text-uppercase" href="http://localhost:3000/">Rick and Morty app</a>
+    </div>
+  </nav>
+</div>
+  )
+}
+
+export default Navbar
