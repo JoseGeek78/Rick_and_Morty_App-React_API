de506a21987ff91723e056c6fe78194ed1c7a292
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-16 20:05:30 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import { useEffect, useState } from 'react';
 import Navbar from './components/Navbar'
+import Characters from './components/Characters';
 
-fetch('https://rickandmortyapi.com/api/character')
-   .then(response => response.json())
 
 function App() {
+
+  const [characters, setcharacters] = useState([])
+
+  const initialUrl = 'https://rickandmortyapi.com/api/character';
+
+  const fetchCharacters = (url) => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setcharacters(data.results))
+      .catch(error => console.log(error))
+  }
+
+  useEffect(() => {
+     fetchCharacters(initialUrl)
+  }, [])
+
   return (
 
   <>
   <Navbar brand='Rick and Morty App (React + API)'/>
+
+   <div className='container'>
+     <Characters characters={characters}/>
+   </div>
     </>
   );
 }
