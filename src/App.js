db5f0b5086a6568a22b76351f7aacfdea6c6aca0
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-16 21:03:19 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import { useEffect, useState } from 'react';
-import Navbar from './components/Navbar'
-import Characters from './components/Characters';
-
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Characters from "./components/Characters";
+import Pagination from "./components/Pagination";
 
 function App() {
+  const [characters, setcharacters] = useState([]);
 
-  const [characters, setcharacters] = useState([])
-
-  const initialUrl = 'https://rickandmortyapi.com/api/character';
+  const initialUrl = "https://rickandmortyapi.com/api/character";
 
   const fetchCharacters = (url) => {
     fetch(url)
-      .then(response => response.json())
-      .then(data => setcharacters(data.results))
-      .catch(error => console.log(error))
-  }
+      .then((response) => response.json())
+      .then((data) => setcharacters(data.results))
+      .catch((error) => console.log(error));
+  };
 
   useEffect(() => {
-     fetchCharacters(initialUrl)
-  }, [])
+    fetchCharacters(initialUrl);
+  }, []);
 
   return (
+    <>
+      <Navbar brand="Rick and Morty App (React + API)" />
 
-  <>
-  <Navbar brand='Rick and Morty App (React + API)'/>
-
-   <div className='container'>
-     <Characters characters={characters}/>
-   </div>
+      <div className="container mt-4">
+        <Pagination />
+        <Characters characters={characters} />
+        <Pagination />
+      </div>
     </>
   );
 }
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,25 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Characters = ({characters}) => {
- return (
-  <div className='row'>
-    {
-    characters.map((item, index) => (
-      <div key={index} className='col'>
-        <div className='card' style={{minWidth: '200px'}}>
-          <img src={item.image} alt='' />
-           <div className='card-body'>
-            <h5 className='cart-title'>{item.name}</h5>
-            <hr />
-            <h6>species: {item.species}</h6>
-            <h6>location: {item.location.name}</h6>
-           </div>
+const Characters = ({ characters }) => {
+  return (
+    <div className="row">
+      {characters.map((item, index) => (
+        <div key={index} className="col mb-4">
+          <div className="card" style={{ minWidth: "200px" }}>
+            <img src={item.image} alt="" />
+            <div className="card-body">
+              <h5 className="cart-title">{item.name}</h5>
+              <hr />
+              <h6>species: {item.species}</h6>
+              <h6>location: {item.location.name}</h6>
+            </div>
+          </div>
         </div>
-        </div>
-      ))
-    }
-  </div>
-  )
-}
+      ))}
+    </div>
+  );
+};
 
-export default Characters
+export default Characters;
