597dc991efaec5db6dc5ba72ec5d513f113cd098
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-16 20:34:03 +0100

    Update

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,12 +1,24 @@
 import React from 'react'
 
 const Characters = ({characters}) => {
-  return (
-    <div className='row'>
-      <div className='col'>
-
-      </div>
-    </div>
+ return (
+  <div className='row'>
+    {
+    characters.map((item, index) => (
+      <div key={index} className='col'>
+        <div className='card' style={{minWidth: '200px'}}>
+          <img src={item.image} alt='' />
+           <div className='card-body'>
+            <h5 className='cart-title'>{item.name}</h5>
+            <hr />
+            <h6>species: {item.species}</h6>
+            <h6>location: {item.location.name}</h6>
+           </div>
+        </div>
+        </div>
+      ))
+    }
+  </div>
   )
 }
 
