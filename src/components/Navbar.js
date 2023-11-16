02dd83ff5e8df13fe2e258e823d6282562af6658
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-16 18:59:23 +0100

    Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = ({ brand }) => {
   return (
 <div>
     <nav className="navbar navbar-dark bg-dark">
     <div className="container">
-      <a className="navbar-brand text-uppercase" href="http://localhost:3000/">Rick and Morty app</a>
+      <a className="navbar-brand text-uppercase" href="/">{brand}</a>
     </div>
   </nav>
 </div>
